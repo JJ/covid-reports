faf63e04931c5ab8344d8e3d262c79f7f35b7b9e
JJ Merelo <jjmerelo@gmail.com>
2020-03-21 10:07:39 +0100

    Processes JH data into a single data frame, refs #2

--- jh-process.R
+++ jh-process.R
@@ -1,7 +1,11 @@
-library(dplyr)
-library(stringr)
-library(RCurl)
+library(tidyr)
 
-raw.world.confirmed <- getURL("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv")
-raw.world.confirmed <- str_replace_all(raw.world.confirmed, '\'', '')
-world.confirmed <- read.table(text = raw.world.confirmed, sep=',',head=T)
+
+base <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-"
+#raw.world.confirmed <- getURL("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv")
+#raw.world.confirmed <- str_replace_all(raw.world.confirmed, '\'', '')
+world.confirmed <- read.csv(paste0(base,"Confirmed.csv"), sep=',',head=T)
+world.confirmed <- gather( world.confirmed, Date, Cases, X1.22.20:X3.20.20)
+world.deaths <- read.csv(paste0(base,"Deaths.csv"), sep=',',head=T)
+world.deaths <- gather( world.deaths, Date, Deaths, X1.22.20:X3.20.20)
+world.data <- merge(world.confirmed,world.deaths,by=c("Province.State","Country.Region","Lat", "Long", "Date"))
