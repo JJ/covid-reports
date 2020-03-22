872c431b3b3301c591d049cfca935e4b703af058
JJ Merelo <jjmerelo@gmail.com>
2020-03-22 20:12:00 +0100

    Scraped data

---
+++ IS/dl-hi-data.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import requests
+import re
+
+url = "https://e.infogram.com/8b33ac5e-6d56-40bc-8d3b-e6a6c941b94e"
+
+response = requests.get(url)
+if response.status_code == 200:
+    wholePage = response.text
+else:
+    exit("Page not found")
+
+scripts = re.search('<script.*>(.+)</script>', wholePage )
+json = re.search('window.infographicData=(.+);',scripts.group(1)).group(1)
+print(json)
