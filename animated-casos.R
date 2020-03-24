9801b156c892d26b40d17b4dcafb8caad927f904
JJ Merelo <jjmerelo@gmail.com>
2020-03-24 10:25:28 +0100

    Trata casos NA!

--- animated-casos.R
+++ animated-casos.R
@@ -2,11 +2,15 @@ library(ggplot2)
 library(gifski)
 library(gganimate)
 
+# Done with help from here https://stackoverflow.com/questions/54855334/gganimate-time-series-and-two-line-plot
+
 load("covid-19-es.Rda")
 data$total <- data$casos - data$salidas
 
 my_plot <- ggplot(data,aes(x = Fecha,y = total ))+
-  geom_point(size = 5) +
+  geom_point(size = 5, color="blue") +
+  geom_segment(aes(xend=max(Fecha), yend = total), linetype=2, colour='pink') +
+  geom_text(aes(x = max(Fecha)+.1, label = sprintf("%5d", total), hjust=0)) +
   transition_states(Fecha, transition_length = 2, state_length = 1) +
   labs(title='Day: {closest_state}')
 
