25c695b462a2b6e0c29af2e7e5b9a372711a9b81
JJ Merelo <jjmerelo@gmail.com>
2020-03-24 09:18:02 +0100

    Código para animación fallido

--- animate.R
+++ animate.R
@@ -1,9 +1,27 @@
 library(ggplot2)
-library(ggthemes)
+library(transformr)
 library(gifski)
 library(gganimate)
+library(tidyr)
 
 load("covid-19-es.Rda")
-casos <- ggplot(data,aes(x=Fecha))+geom_point(aes(y=casos,color="Casos"))+geom_point(aes(y=salidas,color="Salidas"))+theme_tufte()+transition_states(Fecha,transition_length=2,state_length=1)+labs(title='Day: {frame_time}')
-animate(casos, duration = 5, fps = 20, width =800, height = 600, renderer=gifski_renderer())
-anim_save("casos.png")
+data <- gather(data,Tipo,Cuantos,c(casos,salidas))
+
+ggplot(data,aes(x = Fecha, y = Cuantos, group= Tipo, color=Tipo)) + 
+  geom_line() +
+  labs(title='Day: {closest_state}')
+
+my_plot <- ggplot(data,aes(x = Fecha, y = Cuantos, group= Tipo, color=Tipo)) + 
+  geom_line() +
+  transition_reveal(Fecha) + ease_aes("linear")+
+  labs(title='Day: {closest_state}')
+
+animate(
+  plot = my_plot,
+  render = gifski_renderer(),
+  height = 600,
+  width = 800, 
+  duration = 10,
+  fps = 20)
+
+anim_save('gifs/casos-salidas-linea.gif')
