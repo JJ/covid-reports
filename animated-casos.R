d22bd46c9aa1dcc1c2ff5c466f4cfb9b8874148d
JJ Merelo <jjmerelo@gmail.com>
2020-03-24 12:45:04 +0100

    Actualizado con valores de hoy

--- animated-casos.R
+++ animated-casos.R
@@ -5,10 +5,12 @@ library(gganimate)
 # Done with help from here https://stackoverflow.com/questions/54855334/gganimate-time-series-and-two-line-plot
 
 load("covid-19-es.Rda")
-data$total <- data$casos - data$salidas
+data$total <- data$casos - ifelse(is.na(data$salidas),0,data$salidas)
 
-my_plot <- ggplot(data,aes(x = Fecha,y = total ))+
-  geom_point(size = 5, color="blue") +
+my_plot <- ggplot(data,aes(x = Fecha,y = total, color=total ))+
+  geom_point(size = 7) +
+  scale_fill_gradient(name = "count", trans = "log") +
+  shadow_wake(wake_length = 0.1, alpha = FALSE) +
   geom_segment(aes(xend=max(Fecha), yend = total), linetype=2, colour='pink') +
   geom_text(aes(x = max(Fecha)+.1, label = sprintf("%5d", total), hjust=0)) +
   transition_states(Fecha, transition_length = 2, state_length = 1) +
@@ -19,7 +21,7 @@ animate(
   render = gifski_renderer(),
   height = 600,
   width = 800, 
-  duration = 5,
+  duration = 10,
   fps = 20)
 
 anim_save('gifs/totales-point.gif')
