d60be3f0b3db8bedf97873eb72bf8fb79a15d44f
JJ Merelo <jjmerelo@gmail.com>
2020-04-29 12:08:01 +0200

    Actualiza + añade descomposición a 2 semanas

--- anomalize.R
+++ anomalize.R
@@ -5,6 +5,7 @@ load("covid-19-es.Rda")
 data.with.f <- data[as.POSIXct(data$fecha) >= "2020-03-05",]
 fallecimientos.t <- tibble(time=as.POSIXct(data.with.f$fecha),value=data.with.f$Fallecimientos.nuevos)
 fallecimientos.t %>% time_decompose(value) %>% anomalize(remainder) %>% time_recompose()%>% plot_anomaly_decomposition()
+fallecimientos.t %>% time_decompose(value, frequency="2 weeks", trend = "2 weeks") %>% anomalize(remainder) %>% time_recompose()%>% plot_anomaly_decomposition()
 data.minus.1 <- data[-1,]
 casos.t <- tibble(time=as.POSIXct(data.minus.1$fecha),value=data.minus.1$Casos.nuevos)
 casos.t %>% time_decompose(value,frequency='auto',trend='1 weeks') %>% anomalize(remainder) %>% time_recompose()%>% plot_anomaly_decomposition()
