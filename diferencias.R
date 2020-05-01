27519f88d720042d0eb95c91804a43ee8b17c988
JJ Merelo <jjmerelo@gmail.com>
2020-05-01 14:05:01 +0200

    Análisis de las diferencias entre series nuevas y antiguas

---
+++ diferencias.R
@@ -0,0 +1,14 @@
+library(tibble)
+library(dplyr)
+library(anomalize)
+load("covid-19-es.Rda")
+old.data <- read.csv("https://raw.githubusercontent.com/datadista/datasets/master/COVID%2019/old_series/nacional_covid19_old.csv")
+merged.data <- data %>% inner_join(old.data,by="fecha")
+merged.data$diff.casos <- merged.data$casos_total - merged.data$casos.x
+merged.data$diff.fallecimientos <- merged.data$fallecimientos.y - merged.data$fallecimientos.x
+merged.data$fecha <- as.Date("%Y-%m-%d",fecha)
+ggplot(merged.data, aes(x=fecha, y=diff.fallecimientos))+geom_point()+theme_tufte()+theme(axis.text.x = element_text(angle = 90))
+merged.data$diff.casos <- merged.data$casos.y - merged.data$casos_total
+ggplot(merged.data, aes(x=fecha, y=diff.casos))+geom_point()+theme_tufte()+theme(axis.text.x = element_text(angle = 90))
+merged.data$diff.altas <- merged.data$altas.y - merged.data$altas.x
+ggplot(merged.data, aes(x=fecha, y=diff.altas))+geom_point()+theme_tufte()+theme(axis.text.x = element_text(angle = 90))
